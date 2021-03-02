2d222c624f759c4d732859be3ccf7eb1cac5811b
Otelo2 <josean2009@live.com.mx>
2021-03-02 12:15:25 -0600

    Add pipe class

--- pipe.lua
+++ pipe.lua
@@ -1,12 +1,33 @@
 Pipe = Class{}
 
-function Pipe:init()
-    -- load bird image and assign width and height
+--Load the image one time instead of each instance 
+local PIPE_IMAGE = love.graphics.newImage('t.png')
 
+--Speed ad which the pipe scrolls
+PIPE_SPEED = 60
+
+--Dimensions of the pipe image
+PIPE_HEIGHT = 288
+PIPE_WIRDTH = 70
+
+function Pipe:init(orientation, y)
+    -- load bird image and assign width and height
+    self.x = VIRTUAL_WIDTH
+    self.y = y
+
+    self.width = PIPE_IMAGE:getWidth()
+    self.height = PIPE_HEIGHT
+
+    self.orientation = orientation
     --Position bird in the middle of the screen
 
 end
 
+function Pipe:update(dt)
+
 function Pipe:render()
     --draw
+    love.graphics.draw(PIPE_IMAGE, self.x, 
+    (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y),
+    0, 1, self.orientation == 'top' and -1 or 1)
 end
